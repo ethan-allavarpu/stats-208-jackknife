277ceebe876e815ac747147011bccb89e1cd9bf3
Ethan Allavarpu <allavarpuethan@gmail.com>
2023-02-27 11:31:50 -0800

    add meps data

--- scripts/process-data.sh
+++ scripts/process-data.sh
@@ -6,4 +6,6 @@ python src/process-data.py \
         --crime_docs_path data/raw/communities/communities.names \
         --crime_out_path data/processed/crime.csv  \
         --blog_data_path data/raw/BlogFeedback/blogData_train.csv \
-        --blog_out_path data/processed/blog.csv
+        --blog_out_path data/processed/blog.csv \
+        --meps_data_path data/raw/h192.ssp \
+        --meps_out_path data/processed/meps.csv
--- src/process-data.py
+++ src/process-data.py
@@ -2,6 +2,9 @@ import argparse
 import numpy as np
 import pandas as pd
 import re
+import warnings
+
+warnings.filterwarnings("ignore")
 
 argp = argparse.ArgumentParser()
 argp.add_argument("--crime_data_path", type=str, required=True)
@@ -9,6 +12,8 @@ argp.add_argument("--crime_docs_path", type=str, required=True)
 argp.add_argument("--crime_out_path", type=str, required=True)
 argp.add_argument("--blog_data_path", type=str, required=True)
 argp.add_argument("--blog_out_path", type=str, required=True)
+argp.add_argument("--meps_data_path", type=str, required=True)
+argp.add_argument("--meps_out_path", type=str, required=True)
 args = argp.parse_args()
 
 
@@ -76,6 +81,93 @@ def process_blog_data(in_data: str, out_path: str) -> tuple:
     return blog.shape
 
 
+def process_meps_data(in_data: str, out_path: str) -> tuple:
+    """
+    Process BlogFeedback data and write the processed data to a CSV file.
+    Only change is transforming the response: Y = log(1 + # comments)
+
+    Parameters
+    ----------
+    in_data: str
+        File path for input BlogFeedback data
+    out_path: str
+        File path to which we write the processed CSV file
+
+    Returns
+    -------
+    Tuple with the dimensions of the processed dataset
+    """
+    meps = pd.read_sas(in_data, format="xport")
+    # From codebase
+    utilization_vars = [
+        "OBTOTV16",
+        "OBCHIR16",
+        "OBNURS16",
+        "OBOPTO16",
+        "OBASST16",
+        "OBTHER16",
+        "OPTOTV16",
+        "AMCHIR16",
+        "AMNURS16",
+        "AMOPTO16",
+        "AMASST16",
+        "AMTHER16",
+        "ERTOT16",
+        "IPDIS16",
+        "IPNGTD16",
+        "DVTOT16",
+        "HHTOTD16",
+        "RXTOT16",
+    ]
+    categorical_features = [
+        "REGION53",
+        "RACEV2X",
+        "HISPANX",
+        "MARRY53X",
+        "ACTDTY53",
+        "HONRDC53",
+        "LANGSPK",
+        "FILEDR16",
+        "PREGNT53",
+        "WLKLIM53",
+        "WLKDIF53",
+        "AIDHLP53",
+        "SOCLIM53",
+        "COGLIM53",
+        "WRGLAS42",
+        "EMPST53",
+        "MORJOB53",
+        "OCCCT53H",
+        "INDCT53H",
+    ]
+    quantitative_features = [
+        "AGE53X",
+        "EDUCYR",
+        "HIDEG",
+        "FAMINC16",
+        "RTHLTH53",
+        "MNHLTH53",
+        "NOINSTM",
+    ]
+
+    meps = meps[categorical_features + quantitative_features + utilization_vars]
+    meps = meps.astype(int)
+    meps = meps[(meps[utilization_vars] >= 0).all(axis=1)]
+    meps = meps[(meps[categorical_features + quantitative_features] >= -1).all(axis=1)]
+    meps[quantitative_features] = meps[quantitative_features] + (
+        meps[quantitative_features] == -1
+    )
+
+    meps = pd.get_dummies(
+        meps, columns=categorical_features, prefix=categorical_features
+    )
+    meps_x = meps.drop(columns=utilization_vars).reset_index(drop=True)
+    meps_y = np.log(1 + np.array(meps[quantitative_features]).sum(axis=1))
+    meps = pd.concat((meps_x, pd.Series(meps_y)), axis=1, ignore_index=True)
+    meps.to_csv(out_path, index=False)
+    return meps.shape
+
+
 if __name__ == "__main__":
     crime_dim = process_crime_data(
         args.crime_data_path, args.crime_docs_path, args.crime_out_path
@@ -86,3 +178,7 @@ if __name__ == "__main__":
     blog_dim = process_blog_data(args.blog_data_path, args.blog_out_path)
     print(f"Blog CSV file written to {args.blog_out_path}.")
     print(f"Data has {blog_dim[0]} observations, {blog_dim[1] - 1} features")
+
+    meps_dim = process_meps_data(args.meps_data_path, args.meps_out_path)
+    print(f"Blog CSV file written to {args.meps_out_path}.")
+    print(f"Data has {meps_dim[0]} observations, {meps_dim[1] - 1} features")
