34bc84ba448126f5977bf068606f534a304e2a33
Ethan Allavarpu <allavarpuethan@gmail.com>
2023-02-26 20:03:09 -0800

    add documentation

--- src/generate-trial-plots.py
+++ src/generate-trial-plots.py
@@ -11,40 +11,107 @@ argp.add_argument("--output_dir", type=str, required=True)
 argp.add_argument("--n_trials", type=int, required=True)
 args = argp.parse_args()
 
-def get_data_coverage(data_dir: str, data_sources: list, model_types: list, interval_types: list, n_trials: int) -> tuple:
+
+def get_data_coverage(
+    data_dir: str,
+    data_sources: list,
+    model_types: list,
+    interval_types: list,
+    n_trials: int = 20,
+) -> tuple:
     """
     Combine data coverage results from different trials into one object.
 
     Parameters
     ----------
+    data_dir: str
+        Input directory where the data files are stored
+    data_sources: list
+        List of strings with the different data sources/trials
+    model_types: list
+        List of strings with the different models tried
+    interval_types: list
+        List of strings with the different types of intervals considered
+    n_trials: int, default = 20
+        Number of trials run for each model-interval combination
 
+    Returns
+    -------
+    data_coverage: dict
+        Dictionary with each data source as a key and a pd.DataFrame with the
+        following columns as the value: interval_width, Interval Type, Model,
+        Coverage
+    error_lengths: np.array
+        Average and standard deviation (divided by sqrt(n_trials)) for each of
+        the data source, model type, and interval type combinations
     """
     data_coverage = dict()
-    model_dictionary = {"ridge": "Ridge regr.", "rf": "Random for.", "nn": "Neural net."}
-    interval_dictionary = {"naive": "naive", "jackknife": "jackknife", "jackknife_plus": "jackknife+"}
-    error_lengths = np.empty((2, 9, 2, 2))
+    # Convert shorthand abbreviations to nicer names for plotting
+    model_dictionary = {
+        "ridge": "Ridge regr.",
+        "rf": "Random for.",
+        "nn": "Neural net.",
+    }
+    interval_dictionary = {
+        "naive": "naive",
+        "jackknife": "jackknife",
+        "jackknife_plus": "jackknife+",
+    }
+    error_lengths = np.empty((len(data_sources), 9, 2, 2))
     i = 0
     for data_source in data_sources:
         data_df = pd.DataFrame()
         j = 0
         for model_type in model_types:
             for interval_type in interval_types:
-                filename = data_source + "-" + model_type + "-" + interval_type + ".csv"
-                temp_df = pd.read_csv(os.path.join(data_dir, filename))
+                f = "-".join([data_source, model_type, interval_type]) + ".csv"
+                temp_df = pd.read_csv(os.path.join(data_dir, f))
                 temp_df["Interval Type"] = interval_dictionary[interval_type]
                 temp_df["Model"] = model_dictionary[model_type]
-                temp_df["Coverage"] = temp_df["coverage"]
-                error_lengths[i][j][0] = (temp_df.coverage.mean(), temp_df.coverage.std() / np.sqrt(n_trials))
-                error_lengths[i][j][1] = (temp_df.interval_width.mean(), temp_df.interval_width.std() / np.sqrt(n_trials))
+                temp_df.rename(columns={"coverage": "Coverage"}, inplace=True)
+                # Calculate error lengths for plots for coverage, interval width
+                error_lengths[i][j][0] = (
+                    temp_df.Coverage.mean(),
+                    temp_df.Coverage.std() / np.sqrt(n_trials),
+                )
+                error_lengths[i][j][1] = (
+                    temp_df.interval_width.mean(),
+                    temp_df.interval_width.std() / np.sqrt(n_trials),
+                )
                 data_df = pd.concat((data_df, temp_df)).reset_index(drop=True)
                 j += 1
         data_coverage[data_source] = data_df
         i += 1
     return data_coverage, error_lengths
 
-def generate_plots(data_sources: list, output_directory: str, data_coverage: dict, error_lengths: np.array) -> None:
+
+def generate_plots(
+    data_sources: list,
+    output_directory: str,
+    data_coverage: dict,
+    error_lengths: np.array,
+) -> None:
     """
-    Create and save plots that match those on the paper
+    Create and save plots that match those on the paper for each data source
+
+    Parameters
+    ----------
+    data_sources: list
+        List of strings with the different data sources/trials
+    output_directory: str
+        Output directory to which we save the plots
+    data_coverage: dict
+        Dictionary with each data source as a key and a pd.DataFrame with the
+        following columns as the value: interval_width, Interval Type, Model
+        Coverage
+    error_lengths: np.array
+        Average and standard deviation (divided by sqrt(n_trials)) for each of
+        the data source, model type, and interval type combinations
+
+    Returns
+    -------
+    None. Saves plots for each data source in the output directory with the name
+    {data_source}.png
     """
     sns.set(rc={"figure.figsize": (10, 3)})
     sns.set_theme(palette="pastel", style="white")
@@ -53,17 +120,39 @@ def generate_plots(data_sources: list, output_directory: str, data_coverage: dic
         data = data_coverage[data_source]
         _, ax = plt.subplots(1, 2)
         for j, plotted_val in enumerate(["Coverage", "interval_width"]):
-            sns.barplot(data, x="Model", y=plotted_val, hue="Interval Type", errorbar=None, ax=ax[j])
-            ax[j].errorbar(err_x, error_lengths[i, :, j, 0], error_lengths[i, :, j, 1], linewidth=0, color="black", elinewidth=1.5)
+            sns.barplot(
+                data,
+                x="Model",
+                y=plotted_val,
+                hue="Interval Type",
+                errorbar=None,
+                ax=ax[j],
+            )
+            # Manually add error bar
+            ax[j].errorbar(
+                err_x,
+                error_lengths[i, :, j, 0],
+                error_lengths[i, :, j, 1],
+                linewidth=0,
+                color="black",
+                elinewidth=1.5,
+            )
             plt.ylim(0, max(1, data[plotted_val].max()))
             plt.ylabel(plotted_val.replace("_", " ").title())
-            ax[j].legend([],[], frameon=False)
+            ax[j].legend([], [], frameon=False)
+            # Only add coverage rate for coverage plot
             if plotted_val == "Coverage":
                 ax[j].axhline(y=0.9, color="black", linestyle="--", linewidth=1)
         plt.legend(loc=(1.1, 0.35))
-        plt.savefig(os.path.join(output_directory, data_source + ".png"), bbox_inches = "tight", dpi = 300)
+        plt.savefig(
+            os.path.join(output_directory, data_source + ".png"),
+            bbox_inches="tight",
+            dpi=300,
+        )
+        # Clean the plot
         plt.clf()
         plt.close()
+
 
 if __name__ == "__main__":
     data_sources = ["crime", "blog"]
@@ -72,7 +161,7 @@ if __name__ == "__main__":
         data_sources=data_sources,
         model_types=["ridge", "rf", "nn"],
         interval_types=["naive", "jackknife", "jackknife_plus"],
-        n_trials=args.n_trials
+        n_trials=args.n_trials,
     )
     print("Data gathered. Plotting now")
     generate_plots(data_sources, args.output_dir, data_coverage, error_lengths)
